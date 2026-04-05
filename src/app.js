061e0a9f493d891b137f338d6a9faad98247d2c2
Raj Aishwary Pratap Singh <rajaishwaryasingh@gmail.com>
2026-04-06 05:05:34 +0530

    init: project setup with express

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Server is Running"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>
+  console.log(`Server running at port http://localhost:${PORT}`),
+);
