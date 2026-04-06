936688935f62784b6c21f55b5b37738c0c9cc117
Raj Aishwary Pratap Singh <rajaishwaryasingh@gmail.com>
2026-04-06 17:45:58 +0530

    feat: connect mongodb atlas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import connectDB from "./config/db.js";
 
 const app = express();
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => res.send("Server is Running"));
 
